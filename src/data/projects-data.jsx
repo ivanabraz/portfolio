6e76c08a0d3eaf2d82f0027fb05958dd51e53861
Ivana Agustina Braz <ivanabraz@192.168.1.5>
2022-12-08 18:00:32 -0300

    Adding a new project

--- src/data/projects-data.jsx
+++ src/data/projects-data.jsx
@@ -1,6 +1,72 @@
 export const projectsData = 
 [
     {
+        id: "matesuli",
+        categories: ["Academic", "Autonomous"],
+        title: "Matesuli",
+        textIntro: "This work was developed in the first 'Visual Elements of User Interface Design' course of the UX/UI Design Specialization at California Institute of the Arts (CalArts).",
+        text:[
+            "Due to the hard crisis in Latin America, many people —especially youngsters— have been forced to leave their homelands in search of a better future. The uprooting caused by immigration unleashes the need to hold on to certain cultural traditions, which are sometimes difficult to preserve due to the lack of goods. This application is primarily aimed at helping these people to get these supplies, preserve their customs, their identity and build a sense of community acting as a meeting point.", 
+            "You may be wondering what in the world is mate? Maybe you have never heard of it. Well, mate is a caffeine-rich infused drink, that is part of the cultural heritage of some Latin American countries. But it is so much more than just a drink :) Conceptually and emotionally, mate offers a space and a moment of reunion, closeness, togetherness and bonding. Mate is all about the feeling of companionship. Even when you find yourself in total solitude, but in the company of oneself. Mate is mostly shared with other people, might it be friends, family, partners. It is shared in the most literal sense of the word: everybody drinks out of the same mate and the same bombilla — well, it was like that before the pandemic — things have changed a bit since then. Nowadays, people share the moment but not the same mate, we gather around but each one drinks from their own. There are many things that revolve around mate’s universe, like roles; for instance, the cebador is the one who serves and keeps the round going. On the other hand there is also etiquette; for example: water temperature, amount of dust, inclination of the yerba, herbs, food pairing. You have to get it right, otherwise it’s going to taste awful :) To conclude, Mate is the glue that brings people together. That is why it is so important.",
+        ],
+        links: [ 
+            { 
+                linkName:"Visual Elements of User Interface Design - Certification",
+                linkUrl:"https://coursera.org/share/46dcbb230715f6bc1fec4fe01defae2b", 
+            },
+        ],
+        icons: [ 
+            { 
+                iconName:"Illustrator",
+                iconImg:"https://raw.githubusercontent.com/ivanabraz/portfolio/b690d3cf0d5622ad981cab357c1320bd16ec5b33/src/images/logos/skills/illustrator.svg", 
+            },
+            { 
+                iconName:"Photoshop",
+                iconImg:"https://raw.githubusercontent.com/ivanabraz/portfolio/b690d3cf0d5622ad981cab357c1320bd16ec5b33/src/images/logos/skills/photoshop.svg", 
+            },
+        ],
+        furtherInformation: [
+        ],
+        tags: ["Graphic Identity", "UX / UI"],
+        description: "Description",
+        imageThumb:"https://github.com/ivanabraz/portfolio/blob/newdesign/src/images/projects/pokoj/pokoj_header.png?raw=true",
+        imageHeader:"https://github.com/ivanabraz/portfolio/blob/main/src/images/projects/pokoj/pokoj_01.gif?raw=true",
+        images: [
+            { 
+                imgAlt:"Image 1",
+                imgUrl:"https://github.com/ivanabraz/portfolio/blob/main/src/images/projects/pokoj/pokoj_02.gif?raw=true",
+            },
+            { 
+                imgAlt:"Image 2",
+                imgUrl:"https://github.com/ivanabraz/portfolio/blob/main/src/images/projects/pokoj/pokoj_03.gif?raw=true",
+            },
+            { 
+                imgAlt:"Image 3",
+                imgUrl:"https://github.com/ivanabraz/portfolio/blob/main/src/images/projects/pokoj/pokoj_04.gif?raw=true",
+            },
+            { 
+                imgAlt:"Image 4",
+                imgUrl:"https://github.com/ivanabraz/portfolio/blob/main/src/images/projects/pokoj/pokoj_05.gif?raw=true",
+            },
+            { 
+                imgAlt:"Image 5",
+                imgUrl:"https://github.com/ivanabraz/portfolio/blob/main/src/images/projects/pokoj/pokoj_06.gif?raw=true",
+            },
+            { 
+                imgAlt:"Image 6",
+                imgUrl:"https://raw.githubusercontent.com/ivanabraz/portfolio/main/src/images/projects/pokoj/pokoj_07.jpg",
+            },
+            { 
+                imgAlt:"Image 7",
+                imgUrl:"https://raw.githubusercontent.com/ivanabraz/portfolio/main/src/images/projects/pokoj/pokoj_09.jpg",
+            },
+            { 
+                imgAlt:"Image 8",
+                imgUrl:"https://raw.githubusercontent.com/ivanabraz/portfolio/main/src/images/projects/pokoj/pokoj_08.jpg",
+            },
+        ],
+    },
+    {
         id: "pokoj",
         categories: ["Autonomous"],
         title: "Pokój",
@@ -187,51 +253,47 @@ export const projectsData =
         tags: ["Books", "Editorial"],
         description: "Description",
         imageThumb:"https://github.com/ivanabraz/portfolio/blob/main/src/images/projects/el-alma-de-un-gigante/el-alma-de-un-gigante_header.jpg?raw=true",
-        imageHeader:"https://github.com/ivanabraz/portfolio/blob/main/src/images/projects/el-alma-de-un-gigante/el-alma-de-un-gigante_header.jpg?raw=true",
+        imageHeader:"https://github.com/ivanabraz/portfolio/blob/newdesign/src/images/projects/el-alma-de-un-gigante/el-alma-de-un-gigante_header.png?raw=true",
         images: [
             { 
                 imgAlt:"Image 1",
-                imgUrl:"https://github.com/ivanabraz/portfolio/blob/main/src/images/projects/el-alma-de-un-gigante/el-alma-de-un-gigante_01.jpg?raw=true",
+                imgUrl:"https://github.com/ivanabraz/portfolio/blob/newdesign/src/images/projects/el-alma-de-un-gigante/el-alma-de-un-gigante_01.png?raw=true",
             },
             { 
                 imgAlt:"Image 2",
-                imgUrl:"https://github.com/ivanabraz/portfolio/blob/main/src/images/projects/el-alma-de-un-gigante/el-alma-de-un-gigante_02.jpg?raw=true",
+                imgUrl:"https://github.com/ivanabraz/portfolio/blob/newdesign/src/images/projects/el-alma-de-un-gigante/el-alma-de-un-gigante_02.png?raw=true",
             },
             { 
                 imgAlt:"Image 3",
-                imgUrl:"https://github.com/ivanabraz/portfolio/blob/main/src/images/projects/el-alma-de-un-gigante/el-alma-de-un-gigante_03.jpg?raw=true",
+                imgUrl:"https://github.com/ivanabraz/portfolio/blob/newdesign/src/images/projects/el-alma-de-un-gigante/el-alma-de-un-gigante_03.png?raw=true",
             },
             { 
                 imgAlt:"Image 4",
-                imgUrl:"https://github.com/ivanabraz/portfolio/blob/main/src/images/projects/el-alma-de-un-gigante/el-alma-de-un-gigante_04.jpg?raw=true",
+                imgUrl:"https://github.com/ivanabraz/portfolio/blob/newdesign/src/images/projects/el-alma-de-un-gigante/el-alma-de-un-gigante_04.png?raw=true",
             },
             { 
                 imgAlt:"Image 5",
-                imgUrl:"https://github.com/ivanabraz/portfolio/blob/main/src/images/projects/el-alma-de-un-gigante/el-alma-de-un-gigante_05.jpg?raw=true",
+                imgUrl:"https://github.com/ivanabraz/portfolio/blob/newdesign/src/images/projects/el-alma-de-un-gigante/el-alma-de-un-gigante_05.png?raw=true",
             },
             { 
                 imgAlt:"Image 6",
-                imgUrl:"https://github.com/ivanabraz/portfolio/blob/main/src/images/projects/el-alma-de-un-gigante/el-alma-de-un-gigante_06.jpg?raw=true",
+                imgUrl:"https://github.com/ivanabraz/portfolio/blob/newdesign/src/images/projects/el-alma-de-un-gigante/el-alma-de-un-gigante_06.png?raw=true",
             },
             { 
                 imgAlt:"Image 7",
-                imgUrl:"https://github.com/ivanabraz/portfolio/blob/main/src/images/projects/el-alma-de-un-gigante/el-alma-de-un-gigante_07.jpg?raw=true",
+                imgUrl:"https://github.com/ivanabraz/portfolio/blob/newdesign/src/images/projects/el-alma-de-un-gigante/el-alma-de-un-gigante_07.png?raw=true",
             },
             { 
                 imgAlt:"Image 8",
-                imgUrl:"https://github.com/ivanabraz/portfolio/blob/main/src/images/projects/el-alma-de-un-gigante/el-alma-de-un-gigante_08.jpg?raw=true",
+                imgUrl:"https://github.com/ivanabraz/portfolio/blob/newdesign/src/images/projects/el-alma-de-un-gigante/el-alma-de-un-gigante_08.png?raw=true",
             },
             { 
                 imgAlt:"Image 9",
-                imgUrl:"https://github.com/ivanabraz/portfolio/blob/main/src/images/projects/el-alma-de-un-gigante/el-alma-de-un-gigante_09.jpg?raw=true",
+                imgUrl:"https://github.com/ivanabraz/portfolio/blob/newdesign/src/images/projects/el-alma-de-un-gigante/el-alma-de-un-gigante_09.png?raw=true",
             },
             { 
                 imgAlt:"Image 10",
-                imgUrl:"https://github.com/ivanabraz/portfolio/blob/main/src/images/projects/el-alma-de-un-gigante/el-alma-de-un-gigante_010.jpg?raw=true",
-            },
-            { 
-                imgAlt:"Image 11",
-                imgUrl:"https://github.com/ivanabraz/portfolio/blob/main/src/images/projects/el-alma-de-un-gigante/el-alma-de-un-gigante_011.jpg?raw=true",
+                imgUrl:"https://github.com/ivanabraz/portfolio/blob/newdesign/src/images/projects/el-alma-de-un-gigante/el-alma-de-un-gigante_010.png?raw=true",
             },
         ],
     },
