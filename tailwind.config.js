73114d7d91eefa171ab5d80d8bef5c9747d62dd4
Ivana Braz <braz.ivana@gmail.com>
2025-02-21 20:44:43 -0300

    Adding max screen size

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,18 +2,22 @@ const defaultTheme = require('tailwindcss/defaultTheme');
 
 module.exports = {
     content: ["./src/**/*.{html,js,jsx}"],
-    darkMode:'class',
+    darkMode: 'class',
     theme: {
         screens: {
             'xs': '475px',
             ...defaultTheme.screens,
+        },
+        extend: {
+            maxWidth: {
+                'screen-3xl': '1920px', // Ajusta este valor si necesitas otro tamaño
             },
             fontFamily: {
                 'sans': ['TeXGyreHeros'],
                 'body': ['TeXGyreHeros'],
                 'condensed': ['TeXGyreHerosCondensed'],
             },
+        },
     },
-    plugins: [
-    ],
+    plugins: [],
 };
